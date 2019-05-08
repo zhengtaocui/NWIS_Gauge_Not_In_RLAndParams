53f2ed2747516bbb474e4118c5434aacee29f8ff
zhengtaocui <zcui@gmx.com>
2019-05-08 13:44:40 -0400

    Added unit test

---
+++ src/oata/HellowWorldTest.java
@@ -0,0 +1,16 @@
+package oata;
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+
+public class HellowWorldTest {
+	
+	@Test
+	public void testNothing(){}
+	
+	@Test
+	public void testVillAlwaysFail() {
+		fail("An error message");
+	}
+
+}
